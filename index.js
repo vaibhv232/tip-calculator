3f86b41d5154f032bc4cfa0feb8793f2c69a8658
vaibhv232 <108869784+vaibhv232@users.noreply.github.com>
2022-08-13 14:55:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+let numberOfPeople= Number(numberOfPeopleDiv.innerText)
+
+
+const calculateBill=()=> {
+  const bill = Number(billInput.value)
+
+  const tipPercentage=Number(tipInput.value)/100
+
+  const tipAmount = bill* tipPercentage
+
+  const total = tipAmount + bill
+
+  const perPersonTotal = total/numberOfPeople
+  console.log(perPersonTotal)
+
+  perPersonTotalDiv.innerText= `$${perPersonTotal.toFixed(2)}`
+
+}
+const increasePeople = ()=>{
+  numberOfPeople +=1
+  numberOfPeopleDiv.innerText =numberOfPeople
+
+  calculateBill()
+}
+
+
+const decreasePeople=()=>{
+  if(numberOfPeople<=1){
+    throw 'Hey! You cannot have less than 1 person!'
+    return
+  }
+  numberOfPeople-=1;
+
+  numberOfPeopleDiv.innerText =numberOfPeople
+
+  calculateBill()
+}
